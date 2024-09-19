fbd091ad2bde6fec3fc96423d1030656b2c7d92b
Subodh Vashistha <subodh.vashistha@oracle.com>
2024-09-20 00:14:24 +0530

    Added tailwind

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import '../css/output.css'; // Adjust the path as needed
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to Our App!</h1>
+    <div className="p-4">
+      <h1 className="text-3xl font-bold underline">Welcome to Our App!</h1>
       <Link to="/register">
-        <button>Register</button>
+        <button className="mt-2 px-4 py-2 bg-blue-500 text-white rounded">Register</button>
       </Link>
-      <h1>Login to Our App!</h1>
+      <h1 className="text-blue-500 mt-4">Login to Our App!</h1>
       <Link to="/login">
-        <button>Login</button>
+        <button className="mt-2 px-4 py-2 bg-green-500 text-white rounded">Login</button>
       </Link>
     </div>
   );
