12d737dee31723f0dcc3dd1da9099409318cb64d
Subodh Vashistha <subodh.vashistha@oracle.com>
2024-09-20 21:36:50 +0530

    Added Role in User model

--- backend/db/userModel.js
+++ backend/db/userModel.js
@@ -1,17 +1,22 @@
 const mongoose = require("mongoose");
+
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: [true, "Please provide an Email!"],
-        unique: [true, "Email Exist"],
-      },
-    
-      password: {
-        type: String,
-        required: [true, "Please provide a password!"],
-        unique: false,
-      },
-  })
+  email: {
+    type: String,
+    required: [true, "Please provide an Email!"],
+    unique: [true, "Email Exist"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password!"],
+    unique: false,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin", "moderator"],  // You can add more roles as needed
+    default: "user",  // Default role is "user"
+    required: true,
+  }
+});
 
 module.exports = mongoose.model.Users || mongoose.model("Users", UserSchema);
-  
