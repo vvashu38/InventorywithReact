5882d108ae78c01e426a401a59981fb2ffa00a0a
Subodh Vashistha <vashistha3812@gmail.com>
2024-11-15 22:40:51 +0530

    added time out

--- frontend/src/components/AuthContext.js
+++ frontend/src/components/AuthContext.js
@@ -1,7 +1,9 @@
 import React, { createContext, useState, useEffect, useMemo } from 'react';
 import Cookies from "universal-cookie";
+// import { useNavigate } from 'react-router-dom';
 import { jwtDecode } from "jwt-decode";
 const AuthContext = createContext();
+
 
 const AuthProvider = ({ children }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -9,15 +11,30 @@ const AuthProvider = ({ children }) => {
   const [userEmail, setUserEmail] = useState('');
   // Wrap cookies initialization in useMemo to prevent recreating on each render
   const cookies = useMemo(() => new Cookies(), []);
-
+  
+  // const navigate = useNavigate();
   useEffect(() => {
     const token = cookies.get('TOKEN');
     if (token) {
       try {
         const decodedToken = jwtDecode(token);
-        setUserRole(decodedToken.role);
-        setUserEmail(decodedToken.userEmail);
-        setIsLoggedIn(true);
+        const currentTime = Date.now() / 1000; // Current time in seconds
+        // const expirationTime = new Date(decodedToken.exp * 1000); // Convert to milliseconds
+        // const currentTime = expirationTime.getTime() + 5000;
+
+        if (decodedToken.exp < currentTime) {
+          cookies.remove('TOKEN', { path: '/' });
+          window.location.reload(true);
+          setIsLoggedIn(false);
+          setUserRole('');
+          
+          // navigate('/login');
+        }else{
+          setUserRole(decodedToken.role);
+          setUserEmail(decodedToken.userEmail);
+          setIsLoggedIn(true);
+        }
+        
       } catch (error) {
         console.error('Error decoding token', error);
         setIsLoggedIn(false);
